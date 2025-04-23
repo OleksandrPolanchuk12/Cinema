46625bfaea4f8db019f591486265eca28eb856bb
OleksandrPolanchuk12 <polanchykoleksandr@gmail.com>
2025-04-23 12:55:53 +0300

    Automatic capacity detection

--- cinema/models.py
+++ cinema/models.py
@@ -6,7 +6,6 @@ class Cinema(models.Model):
     address = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
-    hall = models.ForeignKey(Hall, od_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- hall/models.py
+++ hall/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from cinema.models import Cinema
 
 class Hall(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.IntegerField()
     row = models.JSONField()
     seat = models.JSONField()
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        total = 0
+        for i in self.seat.values:
+            total += len(i)
+        self.capacity = total
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
