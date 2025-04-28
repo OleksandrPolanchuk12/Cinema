e2c0364f85880d9c0261c8c3acd7faca6ca86406
OleksandrPolanchuk12 <polanchykoleksandr@gmail.com>
2025-04-28 11:25:38 +0300

    Add new field

--- show/admin.py
+++ show/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Show
 
 @admin.register(Show)
-class ShowAdmin(admin.ModdelAdmin):
-    list_display = ['movie', 'hall', 'start_data', 'end_data']
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'hall', 'showtimes', 'prices']
     list_filter = ['movie', 'hall']
